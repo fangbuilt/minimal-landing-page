d1d94d251088f1b27f0d2beb3edb2584ea778c51
Bintang Naufal <1bintangnaufal@gmail.com>
2026-02-28 22:15:14 +0700

    what's up?

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { NavigationBar } from "./components/NavigationBar";
+// import { NavigationBar } from "./components/NavigationBar";
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html*/ `
-    ${NavigationBar()}
-    <main>
-        <h1>Lessons and insights from 8 years</h1>
-        <p>
-        Where to grow your business as a photographer: site or social media?
-        </p>
-    </main>
-`;
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html*/ `
+//     ${NavigationBar()}
+//     <main>
+//         <h1>Lessons and insights from 8 years</h1>
+//         <p>
+//         Where to grow your business as a photographer: site or social media?
+//         </p>
+//     </main>
+// `;
